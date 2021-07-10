5c2b8f936f022a8ce8c2fbf90cf9f68c204710d5
OneTachi <n2verz@gmail.com>
2021-07-10 11:41:58 -0400

    add basic room generation

--- Walker.gd
+++ Walker.gd
@@ -8,6 +8,7 @@ var direction = Vector2.RIGHT
 var borders = Rect2()
 var step_hist = []
 var steps_since_turn = 0
+var rooms = []
 
 func _init(starting_position, new_border):
 	assert(new_border.has_point(starting_position))
@@ -17,8 +18,13 @@ func _init(starting_position, new_border):
 
 func walk(steps):
 	for step in steps:
-		if randf() <= .25 or steps_since_turn > 3:
+		if randf() <= .25 or steps_since_turn > 5:
 			change_direction()
+		elif randf() <= .01:
+			if randf() <= .5:
+				place_circle_room(position)
+			else:
+				place_square_room(position)
 		if step():
 			step_hist.append(position)
 		else:
@@ -42,3 +48,36 @@ func change_direction():
 	direction = directions.pop_front()
 	while not borders.has_point(position + direction):
 		direction = directions.pop_front()
+
+func create_room(position, size):
+	return {position = position, size = size}
+
+func place_square_room(position):
+	var size = Vector2(randi() % 6 + 3, randi() % 6 + 3)
+	var top_left = (position - size/2).ceil()
+	rooms.append(create_room(position, size))
+	
+	for x in size.x:
+		for y in size.y:
+			var new_step = top_left + Vector2(x,y)
+			if borders.has_point(new_step):
+				step_hist.append(new_step)
+
+func place_circle_room(position):
+	var size = [Vector2(randi() % 4 + 2, 0), Vector2(randi() % 4 + 4, 1), Vector2(randi() % 4 + 2, 2)]
+	var top_left = (position - Vector2(size[0].x, size[2].y)/2).ceil()
+	rooms.append(create_room(position, size))
+	
+	for z in size:
+		var new_step = top_left + z
+		if borders.has_point(new_step):
+			step_hist.append(new_step)
+
+func get_end_room():
+	var end_room = rooms.pop_front()
+	var start_vector = step_hist.front()
+	for room in rooms:
+		if start_vector.distance_to(room.position) > start_vector.distance_to(end_room.position):
+			end_room = room 
+	return end_room
+	
--- World/World.gd
+++ World/World.gd
@@ -1,28 +1,46 @@
 extends Node2D
 
 onready var tileMap = $TileMap
-onready var borders = Rect2(0, 0, (randi() % 6 + 38), (randi() % 6 + 22))
+onready var borders = Rect2(0, 0, (randi() % 20 + 38), (randi() % 20 + 22))
+
+const Player = preload("res://Player/Player.tscn")
+const Exit = preload("res://World/ExitDoor.tscn")
 
 func _ready():
 	randomize()
 	generate_level()
 
 func generate_level():
-	var walker = Walker.new(Vector2(randi() % int(borders.size.x) + 1, randi() % int(borders.size.y) + 1), borders)
+	var walker = Walker.new(Vector2(abs((randi() % int(borders.size.x) - 1)), abs(randi() % int(borders.size.y) - 1)), borders)
 	
 	for x in range(-2, borders.size.x + 1, 1):
 		for y in range(-2, borders.size.y + 1, 1):
 			tileMap.set_cellv(Vector2(x,y), 0)
 			#yield(get_tree(), "idle_frame")
 	
-	
-	var map = walker.walk(700)
+	var map = walker.walk(randi() % 400 + 800)
+	instance_player(map.front()*32)
+	instance_exit_door(walker.rooms.back().position*32, walker)
 	walker.queue_free()
 	for location in map:
 		tileMap.set_cellv(location, -1)
 		#yield(get_tree(), "idle_frame")
 	tileMap.update_bitmask_region(Vector2(-1, -1), borders.end + Vector2(1, 1))
 
+func reload_level():
+	get_tree().reload_current_scene()
+
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		get_tree().reload_current_scene()
+		reload_level()
+
+func instance_player(position):
+	var player = Player.instance()
+	add_child(player)
+	player.position = position
+
+func instance_exit_door(position, walker):
+	var exit = Exit.instance()
+	add_child(exit)
+	exit.position = walker.get_end_room().position*32
+	exit.connect("leaving_level", self, "reload_level")
